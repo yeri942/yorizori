09a229659d19f3253d5ca8d96b663eca7e9fa06a
김예리 <dpfl0445@gmail.com>
2022-01-28 15:59:16 +0900

    create post영역 구분만 해놓기

---
+++ frontend/src/Components/viewAll/Postzone.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+const Postzone = () => {
+  return (
+    <div>
+      <div>글 1</div>
+      <div>글 2</div>
+      <div>글 3</div>
+    </div>
+  );
+};
+export default Postzone;
