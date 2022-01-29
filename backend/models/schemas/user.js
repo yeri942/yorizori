9e1c44bd7f81bea6000125d421601a4b405f390f
doosanehi <bds0203@gmail.com>
2022-01-29 16:39:37 +0900

    userSchema 수정

--- backend/models/schemas/user.js
+++ backend/models/schemas/user.js
@@ -3,35 +3,28 @@ const { Schema } = require("mongoose");
 const UserSchema = new Schema(
   {
     //유저아메일
-    userEmail: {
+    email: {
       type: String,
       required: true,
       default: null,
     },
     //유저닉네임
-    userNickname: {
+    nickname: {
       required: true,
       type: String,
     },
     //유저 비밀번호
-    userPw: {
+    password: {
       type: String,
       required: true,
     },
-    //유저 프로필 이미지는 직접 db에 저장하는 방법 어떠신가요
-    userProfile: [
+    profile: [
       { image: { type: Buffer, required: true, default: false } },
       { name: { type: String, required: true, default: null } },
     ],
-    //History 컬렉션과 연결
-    historyId: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "History",
-        default: [],
-      },
-    ],
-    //유저 작성한 post나 유저가 좋아요한 게시물들을
+
+    //유저 작성한 post나 유저가 좋아요한 게시물들을 이렇게 따로 관리하는건 어떨지 생각해봤습니다.
+    //이렇게 관리하면 나중에 프론트에서 요청이 하나 들어오면 너무 여러 데이터들을 업데이트 해야해서 별로일까요??
     // userPosts : [{
     //   type: Schema.Types.ObjectId,
     //   ref: "Post",
