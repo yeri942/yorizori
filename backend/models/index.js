e3b807133bf5d44fd9aa987ec3a2cd2faacb292d
doosanehi <bds0203@gmail.com>
2022-01-29 16:33:28 +0900

    indexSchema 작성

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,18 @@
 const mongoose = require("mongoose");
 const UserSchema = require("./schemas/user");
+const PostSchema = require("./schemas/post");
+const LikeSchema = require("./schemas/like");
+const FollowSchema = require("./schemas/follow");
+const CommentSchema = require("./schemas/comment");
 
 const User = mongoose.model("User", UserSchema);
+const Post = mongoose.model("Post", PostSchema);
+const Like = mongoose.model("Like", LikeSchema);
+const Follow = mongoose.model("Follow", FollowSchema);
+const Comment = mongoose.model("Comment", CommentSchema);
 
 exports.User = User;
+exports.Post = Post;
+exports.Like = Like;
+exports.Follow = Follow;
+exports.Comment = Comment;
