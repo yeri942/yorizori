fb559bfb3c3bf686d16f3929528924656a8c8d7e
김예리 <dpfl0445@gmail.com>
2022-01-29 22:58:45 +0900

    update TopNav 높이 수정

--- frontend/src/Components/nav/TopNav.jsx
+++ frontend/src/Components/nav/TopNav.jsx
@@ -1,34 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { use} from "react-dom"
+import { use } from "react-dom";
 const TopNavBox = styled.div`
-  height: 80px;
+  height: 50px;
   width: 100%;
   position: fixed;
   display: flex;
   top: 0;
   background-color: white;
-  border-bottom: 5px solid rgba(255,210,136, .46);
-`
-
-const TopBackBox = styled.img`
-    color: #FCAD2C; 
-    font-size: 24px;
-    position: absolute;
-    left: 12px;
-    top: 28px;
+  border-bottom: 5px solid rgba(255, 210, 136, 0.46);
 `;
 
+const TopBackBox = styled.img`
+  color: #fcad2c;
+  font-size: 24px;
+  position: absolute;
+  left: 12px;
+  top: 14px;
+`;
 
 const TopNav = () => {
   return (
     <TopNavBox>
       <Link to="/">
-          <TopBackBox src="../images/arrow.png" alt="arrow.png"></TopBackBox>
+        <TopBackBox src="../images/arrow.png" alt="arrow.png"></TopBackBox>
       </Link>
     </TopNavBox>
-  )
+  );
 };
 
 export default TopNav;
