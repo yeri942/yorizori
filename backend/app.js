abd9a10849e04b86fcca0d1a1bc8cb82fb8cfad9
이의섭 <leeuiseop@gmail.com>
2022-01-29 15:29:45 +0900

    SignIn, passport-local

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,13 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const MongoStore = require("connect-mongo");
 const passport = require("passport");
+const session = require("express-session")
 const cors = require("cors");
 const dotenv = require("dotenv");
+const passportConfig = require("./passport/index");
 
 dotenv.config();
-console.log("hello test")
 mongoose.connect(process.env.DB_URL);
 
 mongoose.connection.on("connected", () => {
@@ -18,9 +18,11 @@ mongoose.connection.on("connected", () => {
 });
 
 const app = express();
+passportConfig();
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -32,11 +34,28 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+//! express-session에 의존하므로 뒤에 위치해야 함
+app.use(passport.initialize()); // 요청 객체에 passport 설정을 심음
+app.use(passport.session()); // req.session 객체에 passport정보를 추가 저장
+// passport.session()이 실행되면, 세션쿠키 정보를 바탕으로 해서 passport/index.js의 deserializeUser()가 실행하게 한다.
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
