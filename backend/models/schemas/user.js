a2e6446c88376351f7b8e12625de94437d2bac11
doosanehi <bds0203@gmail.com>
2022-01-29 18:50:46 +0900

    user, postSchema 수정

--- backend/models/schemas/user.js
+++ backend/models/schemas/user.js
@@ -7,29 +7,24 @@ const UserSchema = new Schema(
       type: String,
       required: true,
       default: null,
+      unique: true,
     },
     //유저닉네임
     nickname: {
       required: true,
       type: String,
+      unique: true,
     },
     //유저 비밀번호
     password: {
       type: String,
       required: true,
     },
-    profile: [
-      { image: { type: Buffer, required: true, default: false } },
-      { name: { type: String, required: true, default: null } },
-    ],
-
-    //유저 작성한 post나 유저가 좋아요한 게시물들을 이렇게 따로 관리하는건 어떨지 생각해봤습니다.
-    //이렇게 관리하면 나중에 프론트에서 요청이 하나 들어오면 너무 여러 데이터들을 업데이트 해야해서 별로일까요??
-    // userPosts : [{
-    //   type: Schema.Types.ObjectId,
-    //   ref: "Post",
-    //   default: [],
-    // }]
+    //유저 프로필 사진이 저장된 파일 경로
+    profileImage: {
+      type: String,
+      default: null,
+    },
   },
   { timestamps: true }
 );
