a7543187d096820854df18459dd578569603df6d
doosanehi <bds0203@gmail.com>
2022-01-31 14:06:50 +0900

    asyncHandler 추가(에러처리 핸들러)

---
+++ backend/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+module.exports = (requestHandler) => {
+  return async (req, res, next) => {
+    try {
+      await requestHandler(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
