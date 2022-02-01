52f53ee1f1ff3609087d18f115066b7bca0dc4a1
doosanehi <bds0203@gmail.com>
2022-02-01 16:41:13 +0900

    likeAPI 수정

--- backend/routes/like.js
+++ backend/routes/like.js
@@ -1,51 +1,63 @@
 const express = require("express");
 const router = express.Router();
 const { isLoggedIn } = require("./middlewares");
-const { Like, Post, User } = require("../models/");
-//user 모델 맞춰야함
-// const User = require("../models/User");
+const { Like, Post } = require("../models/");
+const User = require("../models/User");
 const asyncHandler = require("../utils/asyncHandler");
 
-// 저희 postId를 _id 말고 따로 설정할거라면 post schem에 postId (ex: shortId) 도 추가해야 할 것 같네요.
-// 일단 shortId를 postId 쓴다고 생각하고 작업했습니다.
-
-//좋아요 눌렀을 때
+//좋아요 눌렀을 때 post
 router.post(
-  "/posts/:postId/likes",
+  "/",
   isLoggedIn,
-  asyncHandler(async (res, req, next) => {
-    const { postId } = req.params; // url에서 postId 받고 ,
-    const { shortId } = req.user; //req.user 에서 shortId 꺼내고
-    const user = await User.findOne({ shortId });
-    const post = await Post.findOne({ postId });
+  asyncHandler(async (req, res, next) => {
+    const { postId } = req.body; // body에서 postId 받고 ,
+    const { id: userId } = req.user; //req.user 에서 userId 꺼내고
 
-    // 연결된 유저와 게시글이 존재한다면 좋아요가 눌려있다는 것이므로 다시 요청이 들어오면 졿아요를 취소합니다.
-    if (user && post) {
-      await Like.deleteOne({ userId: shortId, postId });
-      post.likesCount -= 1;
-      await post.save(); //좋아요 데이터를 삭제하고 포스트의 좋아요 수를 -1 해줍니다.
-
-      return res.status(200).json({ message: "좋아요가 취소되었습니다." });
+    //만약 이미 좋아요를 누른 게시물이라면 에러를 던짐 ... 이 부분을 서버에서 검증해야 할까요?
+    const currentLike = await Like.findOne({ postId, userId, isUnliked: false });
+    if (currentLike) {
+      throw Error("이미 좋아요 한 게시글입니다.");
+      return;
     }
+    await Like.create({ userId, postId }); //새로운 좋아요 데이터 생성
 
-    await Like.create({ userId: shortId, postId, user: user._id, post: post._id }); //새로운 좋아요 데이터 생성
-    post.likesCount += 1; //해당 포스트에 좋아요 횟수 하나 추가
-    await post.save();
+    // post 스키마에 likeCount 나 commentsCount 같은 카운팅 프로퍼티는 빼는 게 나을 것 같습니다.
+    // post.likesCount += 1; 해당 포스트에 좋아요 횟수 하나 추가
+    // await post.save();
     res.status(200).json({ message: "좋아요 한 목록에 추가되었습니다." });
   })
 );
 
 //게시글에 눌린 좋아요를 조회하는 경우 좋아요를 누른 유저들을 배열로 보내줍니다.
 router.get(
-  "posts/:postId/likes",
+  "/:postId",
   isLoggedIn,
   asyncHandler(async (req, res, next) => {
     const { postId } = req.params; //url에서 postId 받고
-    const { shortId } = req.user; //req.user 에서 shortId 꺼내고
-
-    const likeUserList = await Like.find({ postId }).populate("user"); //해당 포스트의 좋아요만 필터링하고 각각 좋아요를 누른 유저의 정보를 받아옴
-    const newList = likeUserList.map(({ email, nickname }) => ({ email, nickname })); // 유저가 담긴 배열에서 email, nickname만 뽑아서 정리된 배열을 만듦
+    const likeUserList = await Like.find({ postId, isUnliked: false }).populate("userId"); //해당 포스트의 좋아요만 필터링하고 각각 좋아요를 누른 유저의 정보를 받아옴
+    const newList = likeUserList.map(({ userId: { password, ...restUserInfo }, ...restInfo }) => ({
+      user: { ...restUserInfo },
+      ...restInfo,
+    })); // 유저가 담긴 배열에서 passport를 제외한 정보 , 그리고 나머지 정보들을 뽑아서 정리된 배열을 만듦
     res.status(200).json(newList); //좋아요를 누른 유저 정보가 담긴 배열을 응답
+  })
+);
+
+//좋아요 삭제 기능 ... db에서 아예 지우는 것이 아니라 isUnliked 값을 true로 바꾸무
+router.delete(
+  "/",
+  isLoggedIn,
+  asyncHandler(async (req, res, next) => {
+    const { postId } = req.body;
+    const { id: userId } = req.user;
+    const currentLike = await Like.findOne({ postId, userId, isUnliked: false }); //
+    if (!currentLike) {
+      throw new Error("좋아요 한 게시글이 없습니다."); // 종아요한 게시글이 없으면 에러 던짐
+      return;
+    }
+    currentLike.isUnliked = true;
+    currentLike.save();
+    res.status(200).json({ message: "좋아요 한 목록에서 삭제되었습니다." });
   })
 );
 
