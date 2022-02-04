0089037d1bb2ed5f29eb792609c82eb19de07823
EUMDOMI <sandy131712@gmail.com>
2022-02-04 10:43:11 +0900

    update 레시피 수정 구현

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -36,4 +36,18 @@ router.get("/:postId", async (req, res, next) => {
   }
 });
 
+//레시피 수정
+router.post("/:postId", isLoggedIn, async (req, res, next) => {
+  const { postId } = req.params;
+  try {
+    const posts = await Post.findByIdAndUpdate(postId, req.body, {
+      new: true, // new 값이 ture이면 업데이트된 데이터를 반환.
+      // false 이면 업데이트 되기 전 데이터를 반환
+    });
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
