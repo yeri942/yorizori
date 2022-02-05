90756cb127132d01248b1b43c8d7287bd3220a6b
doosanehi <bds0203@gmail.com>
2022-02-05 19:22:08 +0900

    최근 본 게시물 post API

---
+++ backend/routes/history.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const { isLoggedIn } = require("./middlewares");
+const { History } = require("../models/");
+const User = require("../models/User");
+const asyncHandler = require("../utils/asyncHandler");
+
+//게시글 상세페이지 들어갔을 때 history post
+router.post(
+  "/",
+  isLoggedIn,
+  asyncHandler(async (req, res, next) => {
+    const { id: userId } = req.user;
+    const { postId } = req.body;
+    //기존에 봤던 게시물이라면 islastviewed를 false로 바꿔줌
+    const preLastView = await History.findOne({ userId, postId, isLastViewed: true });
+    if (preLastView) {
+      preLastView.isLastViewed = false;
+      await preLastView.save();
+    }
+    await History.create({ userId, postId });
+    res.status(200).json({ message: "마지막으로 본 게시글에 추가되었습니다" });
+  })
+);
+
+module.exports = router;
