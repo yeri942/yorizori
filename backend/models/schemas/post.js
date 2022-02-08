bb6834751d4bc54db709c8082b2c964053f2cd20
EUMDOMI <sandy131712@gmail.com>
2022-02-08 17:31:54 +0900

    update 레시피 수정 구현완료(레시피명, 사진필드3개)

--- backend/models/schemas/post.js
+++ backend/models/schemas/post.js
@@ -8,75 +8,78 @@ const PostSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    //요리 소개
-    desc: {
-      type: String,
-      required: true,
-    },
-    //재료소개
-    ingredient: [
-      {
-        ingreName: { type: String, required: true },
-        ingreCount: { type: String, required: true },
-      },
-    ],
-    //양념소개
-    seasoning: [
-      {
-        ingreName: { type: String, required: true },
-        ingreCount: { type: String, required: true },
-      },
-    ],
-    //조리과정
-    process: { type: Array, required: true },
-    //process_time은 분따로 초따로 (ex) min:1 sec:20 저장
-    processTime: [
-      {
-        min: { type: Number },
-        sec: { type: Number },
-      },
-    ],
+    // //요리 소개
+    // desc: {
+    //   type: String,
+    //   required: true,
+    // },
+    // //재료소개
+    // ingredient: [
+    //   {
+    //     ingreName: { type: String, required: true },
+    //     ingreCount: { type: String, required: true },
+    //   },
+    // ],
+    // //양념소개
+    // seasoning: [
+    //   {
+    //     ingreName: { type: String, required: true },
+    //     ingreCount: { type: String, required: true },
+    //   },
+    // ],
+    // //조리과정
+    // process: { type: Array, required: true },
+    // //process_time은 분따로 초따로 (ex) min:1 sec:20 저장
+    // processTime: [
+    //   {
+    //     min: { type: Number },
+    //     sec: { type: Number },
+    //   },
+    // ],
     //조리과정 이미지
     processImage: [{ type: String, required: true }],
+    processImage_key: [{ type: String, default: null }],
     // 썸네일
     thumbnail: { type: String, required: true },
+    thumbnail_key: { type: String, default: null },
     //완성 사진을 받는 부분입니다.
     doneImage: [{ type: String }],
-    //종류별
-    category: {
-      type: String,
-      required: true,
-    },
-    //상황별 :
-    condition: {
-      type: String,
-      required: true,
-    },
-    //재료별
-    material: {
-      type: String,
-      required: true,
-    },
-    //방법별
-    cook: {
-      type: String,
-      required: true,
-    },
-    //인원수
-    servings: {
-      type: String,
-      required: true,
-    },
-    //요리 시간
-    time: {
-      type: String,
-      required: true,
-    },
-    //난이도
-    diffic: {
-      type: String,
-      required: true,
-    },
+    doneImage_key: [{ type: String, default: null }],
+    // //종류별
+    // category: {
+    //   type: String,
+    //   required: true,
+    // },
+    // //상황별 :
+    // condition: {
+    //   type: String,
+    //   required: true,
+    // },
+    // //재료별
+    // material: {
+    //   type: String,
+    //   required: true,
+    // },
+    // //방법별
+    // cook: {
+    //   type: String,
+    //   required: true,
+    // },
+    // //인원수
+    // servings: {
+    //   type: String,
+    //   required: true,
+    // },
+    // //요리 시간
+    // time: {
+    //   type: String,
+    //   required: true,
+    // },
+    // //난이도
+    // diffic: {
+    //   type: String,
+    //   required: true,
+    // },
     //작성글을 유저와 연결합니다
     userId: {
       type: Schema.Types.ObjectId,
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -34,7 +34,33 @@ router.post(
       done_contents.forEach((done_contents) => arr_done.push(done_contents.location));
       doneImage = arr_done;
 
-      await Post.create({ userId, recipeName, thumbnail, processImage, doneImage });
+      //이미지의 key값 입력
+      thumbnail_key = req.files.thumbnail[0].key;
+
+      //processImage 의 key 값 찾기
+      let arr_process_key = [];
+      let process_contents_key = req.files.processImage;
+      process_contents_key.forEach((process_contents) =>
+        arr_process_key.push(process_contents.key)
+      );
+      processImage_key = arr_process_key;
+
+      //doneImage의 key값 찾기
+      let arr_done_key = [];
+      let done_contents_key = req.files.doneImage;
+      done_contents_key.forEach((done_contents) => arr_done_key.push(done_contents.key));
+      doneImage_key = arr_done_key;
+
+      await Post.create({
+        userId,
+        recipeName,
+        thumbnail,
+        thumbnail_key,
+        processImage,
+        processImage_key,
+        doneImage,
+        doneImage_key,
+      });
 
       res.status(201).json({ message: "레시피등록이 완료되었습니다." });
     } catch (err) {
@@ -67,41 +93,136 @@ router.get("/:postId", async (req, res, next) => {
     res.status(500).json({ message: err.message });
   }
 });
-
 //레시피 수정
-router.patch("/:postId", isLoggedIn, async (req, res, next) => {
-  const { postId } = req.params;
-  try {
-    const posts = await Post.findByIdAndUpdate(postId, req.body, {
-      new: true, // new 값이 ture이면 업데이트된 데이터를 반환.
-      // false 이면 업데이트 되기 전 데이터를 반환
-    });
-    if (!posts) {
-      res.status(404).json({ message: "해당하는 postId가 없습니다." });
-      return;
+router.patch(
+  "/:postId",
+  isLoggedIn,
+  recipeUpload.fields([
+    { name: "thumbnail", maxCount: 1 },
+    { name: "processImage" },
+    { name: "doneImage" },
+  ]),
+  async (req, res, next) => {
+    const { postId } = req.params;
+    try {
+      const posts = await Post.findByIdAndUpdate(postId, req.body, {
+        new: false,
+      });
+      // //thumbnail의 url주소가 이전의 값과 다르다면(변경 되었을 경우)
+      if (posts.thumbnail != req.files.thumbnail[0].location) {
+        //기존의 이미지는 s3에서 삭제
+        const params = {
+          Bucket: "yorijori-recipes-eunjung",
+          Key: posts.thumbnail_key,
+        };
+        s3.deleteObject(params, function (error, data) {
+          if (error) console.log(error);
+          else console.log(data, "요리조리 썸네일 기존 사진 삭제완료");
+          return;
+        });
+
+        //thumbnail 이미지 location DB에 넣기
+        posts.thumbnail = req.files.thumbnail[0].location;
+        posts.thumbnail_key = req.files.thumbnail[0].key;
+      }
+
+      //-------------------------------------------------------
+      //현재 수정하려고 하는 이미지(processImage)의 location값을 받아오기
+      let arr_process = [];
+      let process_contents = req.files.processImage;
+      process_contents.forEach((process_contents) => arr_process.push(process_contents.location));
+
+      //processIamge의 url주소가 이전과 다르다면 변경 되었을 경우
+      if (posts.processImage != arr_process) {
+        //processImage 이미지 location DB에 넣기
+
+        //s3에서 삭제
+        for (i = 0; i < posts.processImage_key.length; i++) {
+          const params = {
+            Bucket: "yorijori-recipes-eunjung",
+            Key: posts.processImage_key[i],
+          };
+          s3.deleteObject(params, function (error, data) {
+            if (error) console.log(error);
+            else console.log(data, "요리조리 processImage 기존 사진 삭제완료");
+            return;
+          });
+        }
+
+        posts.processImage = arr_process;
+        //processImage 의 key 값 찾기
+        let arr_process_key = [];
+        let process_contents_key = req.files.processImage;
+        process_contents_key.forEach((process_contents) =>
+          arr_process_key.push(process_contents.key)
+        );
+        posts.processImage_key = arr_process_key;
+      }
+
+      //-------------------------------------------------------
+      //현재 수정하려고 하는 이미지(doneImage)의 location값을 받아오기
+      let arr_done = [];
+      let done_contents = req.files.doneImage;
+      done_contents.forEach((done_contents) => arr_done.push(done_contents.location));
+      //processIamge의 url주소가 이전과 다르다면 변경 되었을 경우
+      if (posts.doneImage != arr_done) {
+        //s3에서 삭제
+        for (i = 0; i < posts.doneImage_key.length; i++) {
+          const params = {
+            Bucket: "yorijori-recipes-eunjung",
+            Key: posts.doneImage_key[i],
+          };
+          s3.deleteObject(params, function (error, data) {
+            if (error) console.log(error);
+            else console.log(data, "요리조리 doneImage 기존 사진 삭제완료");
+            return;
+          });
+        }
+
+        posts.doneImage = arr_done;
+
+        //doneImage의 key값 찾기
+        let arr_done_key = [];
+        let done_contents_key = req.files.doneImage;
+        done_contents_key.forEach((done_contents) => arr_done_key.push(done_contents.key));
+        posts.doneImage_key = arr_done_key;
+      }
+
+      await posts.save();
+
+      res.status(200).json({ message: req.body });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
     }
-    res.status(200).json(posts);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
   }
-});
+);
 
 //레시피 삭제
-router.delete("/:postId", isLoggedIn, async (req, res, next) => {
-  const { postId } = req.params;
-  try {
-    const posts = await Post.findById(postId).findOne({ useYN: true }); // postId 찾아 삭제
-    if (!posts) {
-      res.status(404).json({ message: "해당하는 postId가 없습니다." });
-      return;
-    }
-    posts.useYN = false;
-    posts.save();
+router.delete(
+  "/:postId",
+  isLoggedIn,
+  recipeUpload.fields([
+    { name: "thumbnail", maxCount: 1 },
+    { name: "processImage" },
+    { name: "doneImage" },
+  ]),
+  async (req, res, next) => {
+    const { postId } = req.params;
+    try {
+      const posts = await Post.findById(postId).findOne({ useYN: true }); // postId 찾아 삭제
+      if (!posts) {
+        res.status(404).json({ message: "해당하는 postId가 없습니다." });
+        return;
+      }
 
-    res.status(200).json({ message: "레시피 삭제가 완료되었습니다." });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+      posts.useYN = false;
+      posts.save();
+
+      res.status(200).json({ message: "레시피 삭제가 완료되었습니다." });
+    } catch (err) {
+      res.status(500).json({ dd: req.files, message: err.message });
+    }
   }
-});
+);
 
 module.exports = router;
