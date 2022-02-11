c64d190f9978b81ee24da72cbcc4c78176409fd5
doosanehi <bds0203@gmail.com>
2022-02-11 23:58:27 +0900

    메인페이지 famousPost 컴포넌트 atom

--- frontend/src/Components/home/homeAtom.jsx
+++ frontend/src/Components/home/homeAtom.jsx
@@ -1,16 +1,60 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+import { getFamousPosts, getFamousPostLikeUser, getFamousPostCommentUserCount } from "./homeAction";
 
-export const FamousPostsAtom = atom({
+export const datailedPostAtom = atom({
+  key: "datailedPostAtom",
+  default: null,
+});
+
+export const detailedPostsLikeUserAtom = atom({
   key: "FamousPostsAtom",
-  default: [],
+  default: null,
 });
 
-export const FamousPostsHeartAtom = atom({
-  key: "FamousPostsHeartAtom",
-  default: {},
+export const FamousPostsSelector = selector({
+  key: "getFamousListSelector",
+  get: async ({ get }) => {
+    try {
+      const famousList = await getFamousPosts(1, 4);
+      return famousList;
+    } catch (err) {
+      throw err;
+    }
+  },
 });
 
-export const FamousPostsCommentAtom = atom({
-  key: "FamousPostsHeartAtom",
-  default: {},
+export const FamousPostLikeUserSelector = selector({
+  key: "FamousPostLikeUserSelector",
+  get: async ({ get }) => {
+    try {
+      const famousPosts = get(FamousPostsSelector);
+      const promises = famousPosts.map(async ({ _id }) => {
+        const user = await getFamousPostLikeUser(_id);
+        return user;
+      });
+      const likeUsers = await Promise.all(promises);
+      // console.log("likeUsers", likeUsers);
+      return likeUsers;
+    } catch (err) {
+      throw err;
+    }
+  },
+});
+
+export const FamousPostCommentUserSelector = selector({
+  key: "FamousPostCommentUserSelector",
+  get: async ({ get }) => {
+    try {
+      const famousPosts = get(FamousPostsSelector);
+      const promises = famousPosts.map(async ({ _id }) => {
+        const userCount = await getFamousPostCommentUserCount(_id);
+        return userCount;
+      });
+      const commentUserCounts = await Promise.all(promises);
+      // console.log("commentUserCounts", commentUserCounts);
+      return commentUserCounts;
+    } catch (err) {
+      throw err;
+    }
+  },
 });
