91e0f19a23b2ecc6210f004be3c23c61b44e8657
Eungwang <eungwang1203@gmail.com>
2022-02-12 23:04:44 +0900

    UPDATE 포스트페이지 카테고리CSS, ALERT수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,21 +15,20 @@ import MoreComments from "./Components/postDetail/MoreComments";
 import KakaoAuth from "./pages/KakaoAuth";
 
 function App() {
-
   return (
     <RecoilRoot>
       <Routes>
         <Route element={<Home />} path="/" />
         <Route element={<LoginPage />} path="/login" />
-        <Route element={<KakaoAuth/>} path="/oauth/kakao/callback" />
+        <Route element={<KakaoAuth />} path="/oauth/kakao/callback" />
         <Route element={<RegisterPage />} path="/register" />
         {/* <Route element={<OldPostPage />} path="/oldpost" /> */}
         <Route element={<ViewAllPage />} path="/view_all" />
-        {/* <Route path="detail" element={<PostDetailPage />} >
+        <Route path="detail" element={<PostDetailPage />}>
           <Route path="comments" element={<MoreComments />} />
-        </Route> */}
-        <Route path="detail/:postId" element={<PostDetailPage />} />
-        <Route path="detail/:postId/comments" element={<MoreComments />} />
+        </Route>
+        {/* <Route path="detail/:postId" element={<PostDetailPage />} />
+        <Route path="detail/:postId/comments" element={<MoreComments />} /> */}
         <Route element={<MyPage />} path="/users/mypage" />
         <Route element={<MyPageEdit />} path="/users/edit" />
         <Route element={<PostPage />} path="/post" />
