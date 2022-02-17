67dde1a3c7bef2c7fcc1682350a30ad29d03cbd9
Eungwang <eungwang1203@gmail.com>
2022-02-17 23:21:53 +0900

    UPDATE Detail페이지

--- frontend/src/Components/postDetail/Comments.jsx
+++ frontend/src/Components/postDetail/Comments.jsx
@@ -10,6 +10,7 @@ import { useRecoilState, useRecoilValue } from "recoil";
 import { commentAtom } from "../../states/comment";
 import Toast from "./Toast";
 import { isLoadingAtom, messageAtom, postIdAtom, toastAtom } from "./toastAtom";
+import { commentScrollStateAtom } from "../../states/detail";
 
 const Comments = () => {
   const [isLoading, setIsLoading] = useRecoilState(isLoadingAtom);
@@ -22,8 +23,19 @@ const Comments = () => {
   const [write, setWrite] = useState("");
   const [toastStatus, setToastStatus] = useRecoilState(toastAtom);
   const [toastMessage, setToastMessage] = useRecoilState(messageAtom);
-
+  const [commentScrollState, setCommentScrollState] = useRecoilState(commentScrollStateAtom);
+  const commentDomRef = useRef(null);
   console.log(comments);
+
+  useEffect(() => {
+    if (commentScrollState === true) {
+      commentDomRef.current.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+    setCommentScrollState(false);
+  }, [commentScrollState]);
+
   useEffect(() => {
     // 원래 useEffect안에는 async-await을 사용하지 못하지만
     // 즉시실행함수로 함수를 만든 후 실행함으로써 해결할 수 있음
@@ -71,7 +83,7 @@ const Comments = () => {
     <>
       {!isLoading && (
         <>
-          <Title>
+          <Title ref={commentDomRef}>
             댓글 <Count>{commentLength}</Count>
           </Title>
           <CommentsWrapper>
@@ -92,10 +104,21 @@ const Comments = () => {
                 </>
               )}
             </form>
-            {comments.filter(comment => comment.isDeleted === false).slice(0, 3).map((comment) => {
-              const isAuth = comment.userId.id === isLogin;
-              return <Comment key={comment._id} comment={comment} isMore={false} isAuth={isAuth} postId={postId} />;
-            })}
+            {comments
+              .filter((comment) => comment.isDeleted === false)
+              .slice(0, 3)
+              .map((comment) => {
+                const isAuth = comment.userId.id === isLogin;
+                return (
+                  <Comment
+                    key={comment._id}
+                    comment={comment}
+                    isMore={false}
+                    isAuth={isAuth}
+                    postId={postId}
+                  />
+                );
+              })}
             {commentLength === 0 ? (
               <EmptyComment>아직 작성된 댓글이 없어요</EmptyComment>
             ) : commentLength > 3 ? (
